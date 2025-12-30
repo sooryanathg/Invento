06c304a1b27ad1e69e22303d162b5a73f0c2f688
UBU11 <uvaisubu11@gmail.com>
2025-12-31 00:24:56 +0530

    style: Hero text aligntment

--- apps/src/app/about/page.tsx
+++ apps/src/app/about/page.tsx
@@ -2,15 +2,20 @@
 
 import Image from "next/image";
 import main from "@/public/main.png";
+import AboutText from "@/src/components/about";
 
 export default function About() {
   return (
-    <div className="bg-white min-w-screen min-h-screen">
-      <Image
-        src={main}
-        className="min-w-screen min-h-screen "
-        alt="Picture of the author"
-      />
-    </div>
+    <section className="bg-white min-w-screen min-h-screen">
+      <div className=" relative">
+        <Image
+          src={main}
+          className="min-w-screen min-h-screen "
+          alt="Picture of the author"
+        />
+
+        <AboutText/>
+      </div>
+    </section>
   );
 }
---
+++ apps/src/components/about.tsx
@@ -0,0 +1,9 @@
+export default function AboutText() {
+  return (
+    <>
+      <div className="absolute top-174  font-bold text-[360px] left-16 opacity-30 text-amber-50">
+        <h1>ABOUT</h1>
+      </div>
+    </>
+  );
+}
