970e36e5bd3cb6cf329b72bcf1b4bf8d6e18b40e
Gayathri-KS101 <gayathrikssrr@gmail.com>
2026-01-21 16:11:05 +0530

    feat: add flower patel animation in comingsoon page

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,5 +1,11 @@
 import ComingSoonPage from "@/src/components/pages/coming-soon/comingSoon";
+import FallingLeaves from "@/src/components/pages/coming-soon/fallingleaves";
 
 export default function ComingSoon() {
-  return <ComingSoonPage />;
+  return (
+    <>
+      <ComingSoonPage />
+      <FallingLeaves />
+    </>
+  );
 }
