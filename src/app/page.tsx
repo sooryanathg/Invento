ec20817b8170f36108f966808dd2e17ebc1f810e
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2026-01-21 18:14:11 +0530

    refactor: Add back FAQ and contact to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function HomePage() {
       <HeroSection />
       <ProShow />
       <Preview />
-      {/*<FAQSection />*/}
-      {/*<ContactSection />*/}
+      <FAQSection />
+      <ContactSection />
     </div>
   );
 }
